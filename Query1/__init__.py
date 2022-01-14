dc203507440d7d4c69afb1d8b6a543273a22fbdb
Giroame <64868509+Giroame@users.noreply.github.com>
2022-01-14 15:41:38 +0100

    First update query 2

--- Query1/__init__.py
+++ Query1/__init__.py
@@ -8,7 +8,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed Query 1.')
 
     name = req.params.get('name')
     if not name:
--- Query2/__init__.py
+++ Query2/__init__.py
@@ -4,7 +4,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed Query 2.')
 
     name = req.params.get('name')
     if not name:
@@ -14,6 +14,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
+    
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver= '{ODBC Driver 17 for SQL Server}'
+
+    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
+    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
+    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
+
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 or len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+        
+    errorMessage = ""
+    dataString = ""
+
+
+
+
+
+
 
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
