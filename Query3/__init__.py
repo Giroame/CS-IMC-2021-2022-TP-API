15b089e3d60a7568a92fa61a82bfd6cb5b5c88e3
giroame <giroame@dellport.localdomain>
2022-01-23 20:58:47 +0100

    update query 3

--- Query3/__init__.py
+++ Query3/__init__.py
@@ -1,10 +1,14 @@
 import logging
-
+from py2neo import Graph
+from py2neo.bulk import create_nodes, create_relationships
+from py2neo.data import Node
+import os
+import pyodbc as pyodbc
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed Query 1.')
 
     name = req.params.get('name')
     if not name:
@@ -14,11 +18,60 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
+    
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver= '{ODBC Driver 17 for SQL Server}'
 
+    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
+    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
+    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
+
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 or len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+        
+    errorMessage = ""
+    dataString = ""
+    ## Average rating by category of films
+    try:
+
+        # Cypher
+        logging.info("Test de connexion avec py2neo...")
+        graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
+        
+        # Rating for each film
+        films = graph.run("MATCH (t:Film) RETURN t.idFilm, t.averageRating")
+
+        for film in films:
+            dataString += f"CYPHER: idFilm={film['t.idFilm']}, averageRating={film['t.averageRating']}\n"
+
+        try:
+            # SQL
+            logging.info("Test de connexion avec pyodbc...")
+            with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+                cursor = conn.cursor()
+
+                cursor.execute("")
+                rows = cursor.fetchall()
+                # for row in rows:
+                #     dataString += f"SQL: tconst={row[0]}, primaryTitle={row[1]}, averageRating={row[2]}\n"
+
+
+        except:
+            errorMessage = "Erreur de connexion a la base SQL"
+    except:
+        errorMessage = "Erreur de connexion a la base Neo4j"
+        
+    
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        nameMessage = f"Hello, {name}!\n"
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        nameMessage = "Le parametre 'name' n'a pas ete fourni lors de l'appel.\n"
+    
+    if errorMessage != "":
+        return func.HttpResponse(dataString + nameMessage + errorMessage, status_code=500)
+
+    else:
+        return func.HttpResponse(dataString + nameMessage + " Connexions réussies a Neo4j et SQL!")
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CS-IMC-2021-2022-TP-API
 
+Query 1 and 2 may not work anymore because of the modification of the databases structure. Query 3, 4 and 5 are adapted to the new DB structure.
+
 # Resource deployment
 ## Infra
 Through terraform, using api.tf in the azure console.
@@ -18,6 +20,54 @@ terraform destroy
 
 ## Code
 CI/CD with github action. Secret added with the "secret" option in github.
+
+
+# Databases
+SQL
+tArtist(idArtist, primaryName, birthYear)
+tFilm(idFilm, primaryTitle, startYear, runtimeMinutes)
+tJob(idArtist, category[enumeration], idFilm)
+tFilmGenre(idFilm, idGenre)
+tGenre(idGenre, genre)
+
+Neo4J
+:Film (averageRating, idFilm, nid, primaryTitle, startYear)
+:Artist(birthYear, idArtist, primaryName)
+
+
+## Example of requests
+Category of films where at least one film had at least one performer had different roles (such as actor and director)
+
+```
+SELECT g.genre FROM tGenre AS g JOIN (
+    SELECT DISTINCT fg.idGenre
+    FROM tJob AS j JOIN tFilmGenre AS fg
+    ON j.idFilm=fg.idFilm
+    GROUP BY j.idArtist, j.idFilm, fg.idGenre
+    HAVING COUNT(DISTINCT j.category) > 1
+) AS costum ON g.idGenre = costum.idGenre
+```
+
+Average rating by category of films
+
+Impossible with the new database structure. See Query 3. 
+
+With another database structure:
+SQL
+tGenres(tconst, genre)
+tNames(nconst, primaryName, birthYear[int])
+tPrincipals(nconst,category,tconst)
+tTitles(tconst, primaryTitle, startYear[int], averageRating[decimal], runtimeMinutes[int])
+
+This request would have worked:
+
+SELECT g.genre, AVG(t.averageRating) AS avgRating 
+FROM tGenres AS g JOIN tTitles AS t ON g.tconst=t.tconst 
+WHERE t.averageRating IS NOT NULL 
+GROUP BY g.genre
+
+
+
 
 
 
